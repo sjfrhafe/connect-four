824c1fdb435f1994e5d45d9a7080defcbe43f417
Fred <frederik.hafemann@gmail.com>
2022-05-30 20:31:46 +0200

    added session and config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlayerModule } from './player/player.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    PlayerModule, 
+      ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const session = require("express-session")
+const RedisStore = require("connect-redis")(session)
+const { createClient } = require("redis")
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService)
+
+  app.setGlobalPrefix('api');
+
+  const redisClient = createClient({
+    legacyMode: true, 
+    url: configService.get('REDIS_URL')
+  })
+  await redisClient.connect()
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      saveUninitialized: true,
+      secret: configService.get('SESSION_SECRET'),
+      resave: false,
+    })
+  )
+
   await app.listen(3000);
+
+
 }
 bootstrap();
