bed5e25cdb95422ee67328a29584f221bfd45aee
Fred <frederik.hafemann@gmail.com>
2022-06-05 16:07:14 +0200

    added sidebar

---
+++ frontend/src/components/PlayerListItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='player-list-item'>
+      <img class="avatar" src="@/assets/avatar_dummy.png" alt="">
+      <div class='display-name'>{{displayName}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['player'], 
+    computed: {
+        displayName(){
+            return this.player ? this.player.name : 'anonymous'
+        }, 
+        avatarUrl(){
+            return this.player ? this.player.avatar : '@/assets/avatar_dummy.png'
+        }
+    }
+}
+</script>
+
+<style>
+.player-list-item{
+    display: flex;
+    color: white;
+    padding: 4pt 10pt;
+    background-color: #222;
+    border-radius: 4pt;
+    align-content: center;
+    align-items: center;
+    margin-bottom: 4pt;
+    transition: background-color 200ms ease;
+    user-select: none;
+}
+
+.player-list-item:hover{
+    background-color: #333;
+}
+
+.avatar{
+    height: 2em;
+}
+
+.display-name{
+    margin-left: 1em;
+}
+</style>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="sidebar">
+      <h2>invite</h2>
+      <div class="input-group mb-3">
+            <b-input :value='$store.getters.key' size='sm' class='mb-2'/>
+            <b-button block variant='primary' size='sm'>Copy Link</b-button>
+      </div>
+      <hr>
+      <h2>players</h2>
+      <player-list />
+      <div class="input-group mt-auto">
+        <b-button varinat='dark' block size='sm'>Report Error</b-button>
+        <b-button variant='danger' block size='sm'>Leave Room</b-button>
+      </div>
+  </div>
+</template>
+
+<script>
+import PlayerList from './PlayerList.vue'
+
+export default {
+    components: { 
+        PlayerList
+    }
+}
+</script>
+
+<style>
+.sidebar{
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0; 
+    left: 0; 
+    background-color: #222;
+    width: 170pt;
+    padding: 10pt;
+    height: calc(100vh - 20pt);
+    text-align: left;
+    color: white;
+}
+
+.sidebar h2{
+    margin: 0 10pt 4pt;
+}
+
+.input-group{
+    padding: 0 10pt;
+}
+</style>
